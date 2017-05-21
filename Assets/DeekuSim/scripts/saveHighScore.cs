f69ffb86447506301c3c5fe0678f932ac64c19f7
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-05-21 14:07:29 +0300

    Fixed ssl errors

--- Assets/DeekuSim/scripts/saveHighScore.cs
+++ Assets/DeekuSim/scripts/saveHighScore.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Net;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 public class saveHighScore : MonoBehaviour {
 
@@ -22,6 +25,8 @@ public class saveHighScore : MonoBehaviour {
       if(statusText != null) {
         statusText.text = "Saving";
       }
+
+      ServicePointManager.ServerCertificateValidationCallback = certificateValidationCallback;
       HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create (scoreServerUrl + "/" + scoreServerApiKey);
       httpWebRequest.ContentType = "application/json";
       httpWebRequest.Method = "POST";
@@ -41,5 +46,24 @@ public class saveHighScore : MonoBehaviour {
         Destroy (saveButton);
       }
     }
+  }
+
+  public bool certificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+    bool isOk = true;
+    if (sslPolicyErrors != SslPolicyErrors.None) {
+      for (int i=0; i<chain.ChainStatus.Length; i++) {
+        if (chain.ChainStatus [i].Status != X509ChainStatusFlags.RevocationStatusUnknown) {
+          chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+          chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+          chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan (0, 1, 0);
+          chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+          bool chainIsValid = chain.Build ((X509Certificate2)certificate);
+          if (!chainIsValid) {
+            isOk = false;
+          }
+        }
+      }
+    }
+    return isOk;
   }
 }
