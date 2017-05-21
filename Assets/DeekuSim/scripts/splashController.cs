a417997f0eced419e2c311b5173244a8835a4881
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-05-21 11:42:16 +0300

    improved splash fade in

--- Assets/DeekuSim/scripts/splashController.cs
+++ Assets/DeekuSim/scripts/splashController.cs
@@ -2,24 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class splashController : MonoBehaviour {
 
   private RawImage image;
   public int nextLevel;
-  private float currentBrightness;
   public float fadeInSpeed;
+  public float fullLogoDelay;
+
+  private float currentBrightness;
+  private float fullLogoVisibleSince;
 
 	void Start () {
     image = gameObject.GetComponent ("RawImage") as RawImage;
     currentBrightness = 0f;
+    fullLogoVisibleSince = 0f;
 	}
 
   void Update() {
     if (currentBrightness < 1f) {
       currentBrightness += Time.deltaTime * fadeInSpeed;
     } else {
-      //Load next level
+      fullLogoVisibleSince += Time.deltaTime * 1000;
+      if (fullLogoVisibleSince > fullLogoDelay) {
+        SceneManager.LoadScene(nextLevel);
+      }
     }
     image.color = new Color (currentBrightness, currentBrightness, currentBrightness, currentBrightness);
   }
