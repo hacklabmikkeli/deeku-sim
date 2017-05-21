b00608174ff657549de625ca6a121d6582247d2c
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-05-21 11:30:30 +0300

    Added fadein script

---
+++ Assets/DeekuSim/scripts/splashController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class splashController : MonoBehaviour {
+
+  private RawImage image;
+  public int nextLevel;
+  private float currentBrightness;
+  public float fadeInSpeed;
+
+	void Start () {
+    image = gameObject.GetComponent ("RawImage") as RawImage;
+    currentBrightness = 0f;
+	}
+
+  void Update() {
+    if (currentBrightness < 1f) {
+      currentBrightness += Time.deltaTime * fadeInSpeed;
+    } else {
+      //Load next level
+    }
+    image.color = new Color (currentBrightness, currentBrightness, currentBrightness, currentBrightness);
+  }
+}
