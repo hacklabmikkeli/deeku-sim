df69a7bb6f0155e854eeae8143436b1994542e2a
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-05-23 17:37:38 +0300

    Added keyboard navigation for linux since mouse clicks dont work in fullscreen mode

---
+++ Assets/DeekuSim/scripts/gameOverKeyboardListener.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class gameOverKeyboardListener : MonoBehaviour {
+
+  public int menuSceneIndex;
+  public InputField nameInput;
+
+  private saveHighScore savehighScoreHandler;
+	// Use this for initialization
+	void Start () {
+    savehighScoreHandler = gameObject.GetComponent ("saveHighScore") as saveHighScore;
+	}
+	
+  void OnGUI() {
+    GUI.FocusControl ("playerName");
+  }
+
+	// Update is called once per frame
+	void Update () {
+	
+    if (Input.GetKeyDown (KeyCode.Alpha1)) {
+      SceneManager.LoadScene (menuSceneIndex);
+    }
+
+    if (Input.GetKeyDown (KeyCode.Alpha2)) {
+      EventSystem.current.SetSelectedGameObject (nameInput.gameObject);
+      nameInput.OnPointerClick(new PointerEventData(EventSystem.current));
+    }
+
+    if (Input.GetKeyDown (KeyCode.Return)) {
+      savehighScoreHandler.saveHighscore ();
+    }
+
+	}
+}
---
+++ Assets/DeekuSim/scripts/menuKeyboardListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuKeyboardListener : MonoBehaviour {
+
+  public int GameSceneIndex;
+
+  private loadHighscores highscoreHandler;
+
+  public void Start() {
+    highscoreHandler = gameObject.GetComponent("loadHighscores") as loadHighscores;
+  }
+
+  public void Update() {
+    if (Input.GetKeyDown (KeyCode.Alpha1)) {
+      SceneManager.LoadScene (GameSceneIndex);
+    }
+
+    if (Input.GetKeyDown (KeyCode.Escape)) {
+      Application.Quit();
+    }
+
+    if (Input.GetKeyDown (KeyCode.Alpha2)) {
+      highscoreHandler.loadHighscore ();
+    }
+
+    if (Input.GetKeyDown (KeyCode.Backspace)) {
+      highscoreHandler.closeHighscore();
+    }
+
+  }
+}
